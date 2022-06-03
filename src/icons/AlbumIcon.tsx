e68961c26a8483d24f4c5949259a39b7a6f13cf2
kurakinasv <svetakurakina2002@gmail.com>
2022-06-03 13:16:53 +0300

    added icons

---
+++ src/icons/AlbumIcon.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react';
+
+import s from './Icons.module.scss';
+
+const ALbumIcon: FunctionComponent = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M0 0h20v20H0V0zm10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-5a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+    </svg>
+  );
+};
+
+export default ALbumIcon;
---
+++ src/icons/PlayIcon.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent } from 'react';
+
+import s from './Icons.module.scss';
+
+const PlayIcon: FunctionComponent = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="60"
+      height="60"
+      viewBox="0 0 24 24"
+      className={s.playicon}
+    >
+      <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-3 17v-10l9 5.146-9 4.854z" />
+    </svg>
+  );
+};
+
+export default PlayIcon;
