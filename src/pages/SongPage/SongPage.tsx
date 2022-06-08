c59a4336c9d324b1ef0b6d6f5994af2aa440acc7
kurakinasv <svetakurakina2002@gmail.com>
2022-06-08 22:14:23 +0300

    SongPage mainly style changes

--- src/pages/SongPage/SongPage.tsx
+++ src/pages/SongPage/SongPage.tsx
@@ -1,9 +1,14 @@
 import { useContext, useEffect, useState } from 'react';
 
 import { SongsContext } from '@app/App';
-import PlayIcon from '@app/icons/PlayIcon';
+import AlbumIcon from '@icons/AlbumIcon';
+import AnnotationsIcon from '@icons/AnnotationsIcon';
+import PlayIcon from '@icons/PlayIcon';
+import ReleaseDateIcon from '@icons/ReleaseDateIcon';
+import ViewsIcon from '@icons/ViewsIcon';
 import { RequestTypes, store } from '@store/store';
 import { SongsContextType } from '@type/types';
+import parse from 'html-react-parser';
 import { Link } from 'react-router-dom';
 
 import s from './SongPage.module.scss';
@@ -13,11 +18,9 @@ const SongPage: React.FC = () => {
   const current = `${songContext.current}`;
   const data = store.currentData;
 
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getSongData = async () => {
-    console.log('getSongData');
-
     try {
       const res: any = await store.reqSearchData(current, RequestTypes.SONG);
 
@@ -25,7 +28,7 @@ const SongPage: React.FC = () => {
         id,
         title,
         artist_names: artist,
-        annotatio_count: annotationCount,
+        annotation_count: annotationCount,
         release_date_for_display: date,
         song_art_image_url: songImg,
         stats,
@@ -44,22 +47,23 @@ const SongPage: React.FC = () => {
         album,
         description,
       };
+
       setIsLoading(false);
     } catch (error: any) {
       console.log('error in songPage:', error.message);
     }
   };
 
-  // const descriptionNormalizer = () => {
-  //   console.log(data?.description?.dom.children[0].children[0]);
-  //   let dom = data?.description?.dom;
-  //   for (let child in dom.children) {
-  //     console.log(child);
-  //   }
-  // };
+  useEffect(() => {
+    getSongData();
+  }, []);
 
-  getSongData().then(() => console.log(store.currentData));
-  // .then(() => descriptionNormalizer());
+  const getSongDescription = () => {
+    // 8 - check for <p>?</p>
+    return data?.description.html.length > 8
+      ? parse(data?.description.html)
+      : '';
+  };
 
   return (
     <>
@@ -68,50 +72,65 @@ const SongPage: React.FC = () => {
           <Link to="/" className={s.arrow}></Link>
           <div className={s.wrapper}>
             <div className={s.image}>
-              <img src={data?.songImg} alt={`${data?.title} cover`} />
+              <div className={s.image__container}>
+                <img src={data?.songImg} alt={`${data?.title} cover`} />
+                <img
+                  src={data?.songImg}
+                  alt={`${data?.title} blur cover`}
+                  className={s.image__background}
+                />
+              </div>
             </div>
 
             <div className={s.info}>
-              <div className={s.title}>{data?.title}</div>
-              <div className={s.artist}>{data?.artist}</div>
+              <div className={s.info__header}>
+                <div className={s.title}>{data?.title}</div>
+                <div className={s.artist}>{data?.artist}</div>
+              </div>
+
               <div className={s.stats}>
-                <div className={s.stats__icon}>
-                  <img src="http://placekitten.com/20" alt="" />
+                <div className={s.stats__text}>
+                  <span title="Release Date">
+                    <ReleaseDateIcon />
+                    <span>{data?.date}</span>
+                  </span>
                 </div>
+
+                <div className={s.stats__text}>
+                  <span title="Album Name">
+                    <AlbumIcon />
+                    <span>{data?.album?.name}</span>
+                  </span>
+                </div>
+
+                <div className={s.stats__text}>
+                  <span title="Pageviews on Genius">
+                    <ViewsIcon />
+                    <span>{data?.stats?.pageviews}</span>
+                  </span>
+                </div>
+
                 {data?.annotationCount! > 0 && (
                   <div className={s.stats__text}>
-                    {data?.annotationCount} annotations
+                    <span title="Annotations on Genius">
+                      <AnnotationsIcon />
+                      <span>{data?.annotationCount}</span>
+                    </span>
                   </div>
                 )}
-                <div className={s.stats__text}>
-                  {data?.stats?.pageviews} pageviews on Genius, released{' '}
-                  {data?.date}
-                </div>
               </div>
-              <div>
-                {data?.album?.name}
-                <img
-                  src={data?.album?.cover_art_url}
-                  alt={`${data?.album?.name} cover`}
-                  width="100px"
-                />
-              </div>
-              <div className={s.listen}>
+
+              <div className={s.listen__button}>
                 <PlayIcon />
                 <span className={s.listen_demo}>Listen demo</span>
               </div>
 
-              {/* <div className={s.description}>
-                Что же есть зло? Ужасное проявление самых отвратительных качеств
-                человека. Именно о таком зле и говорится в песне. Перед нами
-                предстанет картина, в которой молодой человек будет творить
-                ужасные вещи ради получения собственной выгоды, а именно —
-                получение девушки.
-              </div> */}
+              <div className={s.description}>{getSongDescription()}</div>
             </div>
           </div>
         </div>
       )}
+
       {isLoading && <p>Loading...</p>}
     </>
   );
