3dd2249cba29ac516209353a0af9cce4e083c41f
kurakinasv <svetakurakina2002@gmail.com>
2023-01-07 02:15:13 +0300

    "lambda" -> "functions"

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(createProxyMiddleware('/.netlify/functions/', {
+    app.use(proxy('/.netlify/functions/', {
         target: 'http://localhost:5000/',
         "pathRewrite": { "^/\\.netlify/functions": "" }
     }));
